69a3df85b9a39be6dd448365f78091745a4611ad
JALMEJAY KUMAR SHAW <116828650+The-jalmejay@users.noreply.github.com>
2025-08-14 23:05:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
 // Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+	this.make=make;
+	this.model=model;
+}
+Car.prototype.getMakeModel(){
+	return `${make} ${model}`
+}
 
-function SportsCar(make, model, topSpeed) {}
+function SportsCar(make, model, topSpeed) {
+	Car.call(this,make,model);
+	this.topSpeed=topSpeed;
+}
+SportsCar.prototype.getTopSpeed(){
+	return `${topspeed}`
+}
+
+// Create a new SportsCar instance
+const car = new SportsCar("Ferrari", "Testarossa", 200);
+
+// Call getMakeModel() to get the make and model of the car
+console.log(car.getMakeModel()); // Output: Ferrari Testarossa
+
+// Call getTopSpeed() to get the top speed of the sports car
+console.log(car.getTopSpeed()); // Output: 200
+
 
 // Do not change the code below
 window.Car = Car;
